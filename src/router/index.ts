662060baf22dab690899394459fb0ad7271e552b
unknown <rijal.1344@gmail.com>
2024-12-17 10:27:16 +0700

    feat: fetch all user from api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginIndexView from '@/views/login/IndexView.vue'
 import DashboardIndexView from '@/views/dashboard/IndexView.vue'
+import UserIndexView from '@/views/user/IndexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login.index',
-      component: LoginIndexView,
-    },
-    {
-      path: '/dashboard',
       name: 'dashboard.index',
       component: DashboardIndexView,
+    },
+    {
+      path: '/user',
+      name: 'user.index',
+      component: UserIndexView,
     },
   ],
 })
--- src/views/dashboard/IndexView.vue
+++ src/views/dashboard/IndexView.vue
@@ -5,7 +5,7 @@ import DashboardLayout from '@/layouts/DashboardLayout.vue'
   <DashboardLayout>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
-        <p>hello world</p>
+        <p>welcome</p>
       </div>
     </div>
   </DashboardLayout>
